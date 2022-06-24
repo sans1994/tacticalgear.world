324e12a94fc446b6d1d8f3f09e1a2adbb0d84f1f
sans1994 <dprots2016@gmail.com>
2022-06-24 03:06:06 +0300

    1
    1

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -5,7 +5,7 @@
                 <map-block/>
             </tabs-block-item>
             <tabs-block-item header="LIST">
-                <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Consectetur, adipisci velit, sed quia non numquam eius modi.</p>
+                111
             </tabs-block-item>
         </tabs-block>
     </div>
