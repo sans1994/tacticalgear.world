9d96666f9930e43f4015ae9991f76d0ec12eea89
Denys Prots <dprots2016@gmail.com>
2022-07-10 01:52:52 +0300

    Global update. Primevue framework added.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
+import legacy from '@vitejs/plugin-legacy'
 import vue from '@vitejs/plugin-vue'
+import autoprefixer from 'autoprefixer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [
+      vue(),
+      legacy()
+  ],
+  css: {
+    postcss: {
+      plugins: [
+        autoprefixer()
+      ]
+    }
+  }
 })
